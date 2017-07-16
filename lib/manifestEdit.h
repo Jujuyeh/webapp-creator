ef604bb9ca6cd24227e5881ed4381b3221b6979e
Jujuyeh <paul.98.zgz@gmail.com>
2017-07-16 22:54:15 +0200

    Delete manifestEdit.h

--- lib/manifestEdit.h
+++
@@ -1,9 +0,0 @@
-#define MANIFESTEDIT_H
-#ifndef MANIFESTEDIT_H
-
-// The function inserts [name], [desc], [title], [version] and [maint] into
-// the corresponding field of manifest.json.
-void insertManifest(char name[], char desc[], char title[], char version[],
-					 char maint[]);
-					 
-#endif
