7e6f496da70069e0498d48a49168262f12e6db1b
Jujuyeh <paul.98.zgz@gmail.com>
2017-07-16 22:53:58 +0200

    Delete desktopEdit.h

--- lib/desktopEdit.h
+++
@@ -1,9 +0,0 @@
-#define DESKTOPEDIT_H
-#ifndef DESKTOPEDIT_H
-
-// The function inserts the URLs and settings of the webapp into a desktop file.
-void insertDesktop(char name[], char com[], char title[], char url[], int arg[],
-				   char subUrl1[], char subUrl2[], char subUrl3[], int urls[],
-				   char PROVIDER[], char USER_AGENT[], bool https, bool ogra);
-
-#endif
