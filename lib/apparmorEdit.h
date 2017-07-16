b2140fb59b34a4e83e2f6e29a9483e73facc4f78
Jujuyeh <paul.98.zgz@gmail.com>
2017-07-16 22:53:27 +0200

    Delete apparmorEdit.h

--- lib/apparmorEdit.h
+++
@@ -1,7 +0,0 @@
-#define APPARMOREDIT_H
-#ifndef APPARMOREDIT_H
-
-// The function inserts the corresponding policy groups into the apparmor file.
-void insertApparmor(int groups[], char name[]);
-
-#endif
