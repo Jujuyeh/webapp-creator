5e300c04cfda338d234b2dc21b173338ab9df623
Jujuyeh <paul.98.zgz@gmail.com>
2017-08-15 13:13:24 +0200

    Added "validImage"

--- lib/libwebappcreator.h
+++ lib/libwebappcreator.h
@@ -42,3 +42,6 @@ bool URL(char word[]);
 
 //It returns false if [word] starts with "http"
 bool noHttp (char word[]);
+
+//It returns true if word is valid SVG or PNG image file
+bool validImage (char word[]);
