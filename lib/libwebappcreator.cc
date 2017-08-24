46dd5b993037b23edb6b75d2129098c7b98a9102
Jujuyeh <paul.98.zgz@gmail.com>
2017-08-24 18:29:55 +0200

    Fixed Provider and UserAgent

--- lib/libwebappcreator.cc
+++ lib/libwebappcreator.cc
@@ -102,11 +102,11 @@ void insertDesktop(char name[], char com[], char title[], char url[], char arg[]
 	if (!ogra) {
 		f << "webapp-container";
 		if (arg[0]=='1') {f << " --fullscreen";}
-		if (arg[1]=='1') {f << " --accountProvider=" << PROVIDER;}
+		if (arg[1]=='1') {f << " --accountProvider= \"" << PROVIDER << '"';}
 		if (arg[2]=='1') {f << " --accountSwitcher";}
 		if (arg[3]=='1') {f << " --store-session-cookies";}
 		if (arg[4]=='1') {f << " --enable-media-hub-audio";}
-		if (arg[5]=='1') {f << " --user-agent-string=" << USER_AGENT;}
+		if (arg[5]=='1') {f << " --user-agent-string= \"" << USER_AGENT << '"';}
 		if (arg[6]=='1') {f << " --enable-back-forward";}
 		if (arg[7]=='1' && arg[6] == '0') {f << " --enable-addressbar";}
 		if (arg[8]=='1') {f << " --open-external-url-in-overlay";}
